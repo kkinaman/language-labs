c0e95d15e7bee5977b6e78d5c05525cd7baa9b12
Bill Zito <bill.r.zito@gmail.com>
2016-11-02 12:09:22 -0700

    Transcribe and translate speech

--- imports/MediaStreamRecorder.js
+++ imports/MediaStreamRecorder.js
@@ -32,116 +32,10 @@
 // ______________________
 // MediaStreamRecorder.js
 
-module.exports = function(mediaStream) {
-    if (!mediaStream) {
-        throw 'MediaStream is mandatory.';
-    }
-    
-    // void start(optional long timeSlice)
-    // timestamp to fire "ondataavailable"
-    this.start = function (timeSlice) {
-        // Media Stream Recording API has not been implemented in chrome yet;
-        // That's why using WebAudio API to record stereo audio in WAV format
-        // var Recorder = IsChrome || IsEdge || IsOpera ? window.StereoAudioRecorder || IsEdge || IsOpera : window.MediaRecorderWrapper;
-        
-        var Recorder = window.StereoAudioRecorder;
-        // video recorder (in WebM format)
-        if (this.mimeType.indexOf('video') !== -1) {
-            Recorder = IsChrome || IsEdge || IsOpera ? window.WhammyRecorder : window.MediaRecorderWrapper;
-        }
-        
-        // video recorder (in GIF format)
-        if (this.mimeType === 'image/gif') {
-            Recorder = window.GifRecorder;
-        }
-        
-        // allows forcing StereoAudioRecorder.js on Edge/Firefox
-        if (this.recorderType) {
-            Recorder = this.recorderType;
-        }
-        
-        mediaRecorder = new Recorder(mediaStream);
-        mediaRecorder.blobs = [];
-        
-        var self = this;
-        mediaRecorder.ondataavailable = function (data) {
-            mediaRecorder.blobs.push(data);
-            self.ondataavailable(data);
-        };
-        mediaRecorder.onstop = this.onstop;
-        mediaRecorder.onStartedDrawingNonBlankFrames = this.onStartedDrawingNonBlankFrames;
-        
-        // Merge all data-types except "function"
-        mediaRecorder = mergeProps(mediaRecorder, this);
-        
-        mediaRecorder.start(timeSlice);
-    };
-    
-    this.onStartedDrawingNonBlankFrames = function () { };
-    this.clearOldRecordedFrames = function () {
-        if (!mediaRecorder) {
-            return;
-        }
-        
-        mediaRecorder.clearOldRecordedFrames();
-    };
-    
-    this.stop = function () {
-        if (mediaRecorder) {
-            mediaRecorder.stop();
-        }
-    };
-    
-    this.ondataavailable = function (blob) {
-        console.log('ondataavailable..', blob);
-    };
-    
-    this.onstop = function (error) {
-        console.warn('stopped..', error);
-    };
-    
-    this.save = function (file, fileName) {
-        if (!file) {
-            if (!mediaRecorder) {
-                return;
-            }
-            
-            var bigBlob = new Blob(mediaRecorder.blobs, {
-                type: mediaRecorder.blobs[0].type || this.mimeType
-            });
-            
-            invokeSaveAsDialog(bigBlob);
-            return;
-        }
-        invokeSaveAsDialog(file, fileName);
-    };
-    
-    this.pause = function () {
-        if (!mediaRecorder) {
-            return;
-        }
-        mediaRecorder.pause();
-        console.log('Paused recording.', this.mimeType || mediaRecorder.mimeType);
-    };
-    
-    this.resume = function () {
-        if (!mediaRecorder) {
-            return;
-        }
-        mediaRecorder.resume();
-        console.log('Resumed recording.', this.mimeType || mediaRecorder.mimeType);
-    };
-    
-    this.recorderType = null; // StereoAudioRecorder || WhammyRecorder || MediaRecorderWrapper || GifRecorder
-    
-    // Reference to "MediaRecorder.js"
-    var mediaRecorder;
-}
-
 // ______________________
 // MultiStreamRecorder.js
 
-function MultiStreamRecorder(mediaStream) {
+exports.MultiStreamRecorder = function(mediaStream) {
     if (!mediaStream) {
         throw 'MediaStream is mandatory.';
     }
@@ -154,8 +48,8 @@ function MultiStreamRecorder(mediaStream) {
     // void start(optional long timeSlice)
     // timestamp to fire "ondataavailable"
     this.start = function (timeSlice) {
-        audioRecorder = new MediaStreamRecorder(mediaStream);
-        videoRecorder = new MediaStreamRecorder(mediaStream);
+        audioRecorder = new exports.MediaStreamRecorder(mediaStream);
+        videoRecorder = new exports.MediaStreamRecorder(mediaStream);
         
         audioRecorder.mimeType = 'audio/ogg';
         videoRecorder.mimeType = 'video/webm';
@@ -459,7 +353,7 @@ function MediaRecorderWrapper(mediaStream) {
                 return;
             }
             
-            mediaRecorder = new MediaRecorder(mediaStream);
+            mediaRecorder = new exports.MediaRecorder(mediaStream);
             
             mediaRecorder.ondataavailable = function (e) {
                 console.log('ondataavailable', e.data.type, e.data.size, e.data);
@@ -582,7 +476,7 @@ function MediaRecorderWrapper(mediaStream) {
 // ======================
 // StereoAudioRecorder.js
 
-function StereoAudioRecorder(mediaStream) {
+exports.StereoAudioRecorder = function(mediaStream) {
     // void start(optional long timeSlice)
     // timestamp to fire "ondataavailable"
     this.start = function (timeSlice) {
@@ -860,8 +754,7 @@ function StereoAudioRecorderHelper(mediaStream, root) {
 
 // ===================
 // WhammyRecorder.js
-
-function WhammyRecorder(mediaStream) {
+exports.WhammyRecorder = function(mediaStream) {
     // void start(optional long timeSlice)
     // timestamp to fire "ondataavailable"
     this.start = function (timeSlice) {
@@ -1218,7 +1111,7 @@ function WhammyRecorderHelper(mediaStream, root) {
 // --------------
 // GifRecorder.js
 
-function GifRecorder(mediaStream) {
+exports.GifRecorder = function(mediaStream) {
     if (typeof GIFEncoder === 'undefined') {
         throw 'Please link: https://cdn.webrtc-experiment.com/gif-recorder.js';
     }
@@ -1781,3 +1674,109 @@ var Whammy = (function () {
         Video: WhammyVideo
     };
 })();
+
+exports.mediaMaker = function(mediaStream) {
+    if (!mediaStream) {
+        throw 'MediaStream is mandatory.';
+    }
+    
+    // void start(optional long timeSlice)
+    // timestamp to fire "ondataavailable"
+    this.start = function (timeSlice) {
+        // Media Stream Recording API has not been implemented in chrome yet;
+        // That's why using WebAudio API to record stereo audio in WAV format
+        // var Recorder = IsChrome || IsEdge || IsOpera ? window.StereoAudioRecorder || IsEdge || IsOpera : window.MediaRecorderWrapper;
+        
+        var Recorder = exports.StereoAudioRecorder;
+        // video recorder (in WebM format)
+        if (this.mimeType.indexOf('video') !== -1) {
+            Recorder = IsChrome || IsEdge || IsOpera ? exports.WhammyRecorder : exports.MediaRecorderWrapper;
+        }
+        
+        // video recorder (in GIF format)
+        if (this.mimeType === 'image/gif') {
+            Recorder = exports.GifRecorder;
+        }
+        
+        // allows forcing StereoAudioRecorder.js on Edge/Firefox
+        if (this.recorderType) {
+            Recorder = this.recorderType;
+        }
+        
+        mediaRecorder = new Recorder(mediaStream);
+        mediaRecorder.blobs = [];
+        
+        var self = this;
+        mediaRecorder.ondataavailable = function (data) {
+            mediaRecorder.blobs.push(data);
+            self.ondataavailable(data);
+        };
+        mediaRecorder.onstop = this.onstop;
+        mediaRecorder.onStartedDrawingNonBlankFrames = this.onStartedDrawingNonBlankFrames;
+        
+        // Merge all data-types except "function"
+        mediaRecorder = mergeProps(mediaRecorder, this);
+        
+        mediaRecorder.start(timeSlice);
+    };
+    
+    this.onStartedDrawingNonBlankFrames = function () { };
+    this.clearOldRecordedFrames = function () {
+        if (!mediaRecorder) {
+            return;
+        }
+        
+        mediaRecorder.clearOldRecordedFrames();
+    };
+    
+    this.stop = function () {
+        if (mediaRecorder) {
+            mediaRecorder.stop();
+        }
+    };
+    
+    this.ondataavailable = function (blob) {
+        console.log('ondataavailable..', blob);
+    };
+    
+    this.onstop = function (error) {
+        console.warn('stopped..', error);
+    };
+    
+    this.save = function (file, fileName) {
+        if (!file) {
+            if (!mediaRecorder) {
+                return;
+            }
+            
+            var bigBlob = new Blob(mediaRecorder.blobs, {
+                type: mediaRecorder.blobs[0].type || this.mimeType
+            });
+            
+            invokeSaveAsDialog(bigBlob);
+            return;
+        }
+        invokeSaveAsDialog(file, fileName);
+    };
+    
+    this.pause = function () {
+        if (!mediaRecorder) {
+            return;
+        }
+        mediaRecorder.pause();
+        console.log('Paused recording.', this.mimeType || mediaRecorder.mimeType);
+    };
+    
+    this.resume = function () {
+        if (!mediaRecorder) {
+            return;
+        }
+        mediaRecorder.resume();
+        console.log('Resumed recording.', this.mimeType || mediaRecorder.mimeType);
+    };
+    
+    this.recorderType = null; // StereoAudioRecorder || WhammyRecorder || MediaRecorderWrapper || GifRecorder
+    
+    // Reference to "MediaRecorder.js"
+    var mediaRecorder;
+}
--- imports/components/Transcriber.js
+++ imports/components/Transcriber.js
@@ -1,133 +1,96 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import MediaStreamRecorder from '../MediaStreamRecorder';
+import Media from '../MediaStreamRecorder';
 
 class Transcriber extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      results: ''
+      transcript: '',
+      sourceLang: 'en',
+      sourceLangName: "English",
+      targetLang: "es",
+      targetLangName: "Spanish",
+      mediaRecorder: {},
+      stream: {},
+      ws: {}
     }
   }
-  render() {
-    return (
-      <div>
-        <button onClick={() => StartSession()}>Start</button>
-        <button onClick={() => StopSession()}>Stop</button>
-        <div className='output'></div>
-      </div>
-    )
-  }
-}
 
-export default Transcriber;
-
-//Set default settings
-var sourceLang = "en";
-var sourceLangName = "English";
-var targetLang = "es";
-var targetLangName = "Spanish";
-var profanity = "NoAction";
-var textSize = "small";
-
-var activeTabId = "";
-
-var _errFunction = console.error;
-
-//State variables
-var INITIAL_VALUE = -1;
-var ongoingsubtitling = false;
-
-//Connection variables
-var mediaRecorder = null;
-var stream = null;
-var ws = null;
-
-// feature detection 
-if (!navigator.getUserMedia)
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                  navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-
-//Open websocket with URL, set appropriate event listeners and feed in data stream
-function ConnectWithTranslatorServer(lang1, lang2, accToken) {
+  //Open websocket with URL, set appropriate event listeners and feed in data stream
+  ConnectWithTranslatorServer(lang1, lang2, accToken) {
     console.debug("Connecting with service...");
-    
     try {
-        
         //Setup connections
-        mediaRecorder = new MediaStreamRecorder(stream);
-        mediaRecorder.mimeType = 'audio/wav';
-        mediaRecorder.audioChannels = 2;
+        var newRecorder = new Media.mediaMaker(this.state.stream);
+        this.setState({mediaRecorder: newRecorder});
+        this.state.mediaRecorder.mimeType = 'audio/wav';
+        this.state.mediaRecorder.audioChannels = 2;
         console.debug("Created media recorder");
      
         
-        var ws_url = "wss://dev.microsofttranslator.com/speech/translate?api-version=1.0&from=" + lang1 + "&to=" + lang2 + '&profanityaction=' + profanity + "&access_token=" + encodeURIComponent(accToken);
+        var ws_url = "wss://dev.microsofttranslator.com/speech/translate?api-version=1.0&from=" + lang1 + "&to=" + lang2 + "&access_token=" + encodeURIComponent(accToken);
         
-        ws = new WebSocket(ws_url);
+        this.setState({ws: new WebSocket(ws_url)});
         console.debug("Created websocket");
         
-        ws.onopen = function () {
-            mediaRecorder.start(200);
+        this.state.ws.onopen = () => {
+            this.state.mediaRecorder.start(200);
             }
         
-        ws.onclose = function (event) {
+        this.state.ws.onclose = function (event) {
             console.error('web socket closed' + JSON.stringify(event));
         }
         
-        ws.onerror = function (event) {
+        this.state.ws.onerror = function (event) {
             alert('exDescription: Code: ' + event.code + ' Reason: ' + event.reason);
             displayError(5);
         }
         console.debug("Setup websocket callbacks");
         
         
-    } catch (error) {
-        console.error("Error when opening session: " + error);
-        CloseSession();
-    }
-}
+      } catch (error) {
+          console.error("Error when opening session: " + error);
+          CloseSession();
+      }
+  }
 
-//Clean up session in terms of UI, state and connections
-function StopSession() {
-    TerminateConnections(true);
-}
+  StopSession() {
+    this.TerminateConnections(true);
+  }
 
-function TerminateConnections(closeStream) {
+  TerminateConnections(closeStream) {
     
     if (closeStream) {
-        if (stream) {
-            stream = null;
+        if (this.state.stream) {
+            this.state.stream = null;
         }
         else
-            console.error("Stream not set");
+            console.error("this.state.stream not set");
     }
     
-    mediaRecorder ? mediaRecorder.stop() : console.error("No mediaRecorder");
+    this.state.mediaRecorder ? this.state.mediaRecorder.stop() : console.error("No mediaRecorder");
     
-    ws ? ws.close() : console.error("No websocket");
-    ws = null;
+    this.state.ws ? this.state.ws.close() : console.error("No websocket");
+    this.state.ws = null;
     
     console.debug("Terminated connections");
-}
+  }
 
-
-
-function StartSession() {
-    
+  StartSession() {
     if (navigator.getUserMedia) {
-        navigator.getUserMedia({ audio: true }, function (streamArg) {
-            
-            stream = streamArg;
-            if (stream)
+        navigator.getUserMedia({ audio: true }, (streamArg) => {
+            console.log(streamArg);
+            this.setState({stream: streamArg});
+            if (this.state.stream)
             {
                 console.debug("Starting session...");
                 //Update state
-                ongoingsubtitling = true;
+                // ongoingsubtitling = true;
                 // GenerateToken(SetupWebConnection);
                 //TOKEN GOES HERE
-                SetupWebConnection('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6Imh0dHBzOi8vZGV2Lm1pY3Jvc29mdHRyYW5zbGF0b3IuY29tLyIsInN1YnNjcmlwdGlvbi1pZCI6IjIyODMzNmZiMmFiODQ4ZTdhYjBhNzM2YThmOGJjMGRhIiwicHJvZHVjdC1pZCI6IlNwZWVjaFRyYW5zbGF0b3IuRjAiLCJjb2duaXRpdmUtc2VydmljZXMtZW5kcG9pbnQiOiJodHRwczovL2FwaS5jb2duaXRpdmUubWljcm9zb2Z0LmNvbS9pbnRlcm5hbC92MS4wLyIsImF6dXJlLXJlc291cmNlLWlkIjoiL3N1YnNjcmlwdGlvbnMvZDc4MjEwOTgtMDk5Yy00OTExLTk5OTItNGY3YWM1OTYyOGMyL3Jlc291cmNlR3JvdXBzL2xhbmd1YWdlLWxhYnMvcHJvdmlkZXJzL01pY3Jvc29mdC5Db2duaXRpdmVTZXJ2aWNlcy9hY2NvdW50cy9pbnZhbGlkLW1lbW9yaWVzIiwiaXNzIjoidXJuOm1zLmNvZ25pdGl2ZXNlcnZpY2VzIiwiYXVkIjoidXJuOm1zLm1pY3Jvc29mdHRyYW5zbGF0b3IiLCJleHAiOjE0NzgxMDYyMTl9._zgKKabO0K6xbbvdEwR4CerIelQL9QxErmDN4HnQs6Y');
+                this.SetupWebConnection('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6Imh0dHBzOi8vZGV2Lm1pY3Jvc29mdHRyYW5zbGF0b3IuY29tLyIsInN1YnNjcmlwdGlvbi1pZCI6IjIyODMzNmZiMmFiODQ4ZTdhYjBhNzM2YThmOGJjMGRhIiwicHJvZHVjdC1pZCI6IlNwZWVjaFRyYW5zbGF0b3IuRjAiLCJjb2duaXRpdmUtc2VydmljZXMtZW5kcG9pbnQiOiJodHRwczovL2FwaS5jb2duaXRpdmUubWljcm9zb2Z0LmNvbS9pbnRlcm5hbC92MS4wLyIsImF6dXJlLXJlc291cmNlLWlkIjoiL3N1YnNjcmlwdGlvbnMvZDc4MjEwOTgtMDk5Yy00OTExLTk5OTItNGY3YWM1OTYyOGMyL3Jlc291cmNlR3JvdXBzL2xhbmd1YWdlLWxhYnMvcHJvdmlkZXJzL01pY3Jvc29mdC5Db2duaXRpdmVTZXJ2aWNlcy9hY2NvdW50cy9pbnZhbGlkLW1lbW9yaWVzIiwiaXNzIjoidXJuOm1zLmNvZ25pdGl2ZXNlcnZpY2VzIiwiYXVkIjoidXJuOm1zLm1pY3Jvc29mdHRyYW5zbGF0b3IiLCJleHAiOjE0NzgxMTMzNDF9.XdeEaLO-GluDZ8ZTYyFCvM__3TL4enVHP0QSgHio1VQ');
     
             } else {
                console.error("stream is null");
@@ -140,81 +103,15 @@ function StartSession() {
     } 
     else alert('getUserMedia not supported in this browser.');
 
-}
+  }
 
-// REMEMBER: register with ADM and enter your client ID and secret here.
-// This code should always be run on the server, never on the client, so the secret is not exposed to app users
-// function getAdmToken(){
-//     var post_data = querystring.stringify({
-//         'client_id': 'invalid-memories', //your client id,
-//         'scope': 'http://api.microsofttranslator.com',
-//         'grant_type': 'client_credentials',
-//         'client_secret': '5Bpf0b5tDzFUGiFNL8lyLJjEyQKUvN2pQo2j8piaxvo=' // your client secret
-//     });
-    
-//     var post_options = {
-//         host: 'datamarket.accesscontrol.windows.net',
-//         path: '/v2/OAuth2-13',
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//             'Content-Length': Buffer.byteLength(post_data)
-//         }
-//     };
-    
-    
-//     var post_req = https.request(post_options, function (res) {
-//         res.setEncoding('utf8');
-//         if (res.statusCode == 200) {
-//             res.on('data', function (chunk) {
-//                 accToken = JSON.parse(chunk).access_token;
-//                 //log('[generateAdmToken] Success');
-//             });
-//         }
-//         else {
-//             accToken = ERR_REQ_FAIL;
-//             //log('[generateAdmToken] Failed with status code ' + res.statusCode, "error");
-//         }
-//     });
-    
-//     post_req.write(post_data);
-//     post_req.end();
-
-// }
-
-// function GenerateToken(callbackIfSuccessfulToken) {
-//     var accToken = "";
-
-//     var xhttp = new XMLHttpRequest();
-//     // *** USE YOUR OWN SERVER TO RETURN A VALID ADM TOKEN ***
-//     // We suggest using a session cookie for a minimal validation that request for token is coming from your own client app
-//     // For commercial apps, you may want to protect the call to get a token behind your own user authentication.
-//     // xhttp.open("GET", "http://localhost:3000/token", true);
-//     xhttp.open('GET', 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken', true);
-    
-//     xhttp.onreadystatechange = function () {
-//         if (xhttp.readyState == 4 && xhttp.status == 200) {
-//             accToken = xhttp.responseText;
-
-//             console.debug("Token: " + accToken);
-//             if (typeof callbackIfSuccessfulToken === 'function')
-//                 callbackIfSuccessfulToken(accToken);
-//             else
-//                 console.error("callbackIfSuccessfulToken is not a function");
-//         }
-//     }
-    
-//     xhttp.send();
-
-// }
-
-function SetupWebConnection(accToken) {
+  SetupWebConnection(accToken) {
     console.debug("Setting up connection for valid ADM token...");
-    ConnectWithTranslatorServer(sourceLang, targetLang, accToken);
+    this.ConnectWithTranslatorServer(this.state.sourceLang, this.state.targetLang, accToken);
     var firstTranslation = true;
    
     //Handle messages
-    ws.onmessage = function (event) {
+    this.state.ws.onmessage = (event) => {
         console.debug('received event from socket.');
         var response = JSON.parse(event.data);
         console.debug(response.recognition);
@@ -223,42 +120,42 @@ function SetupWebConnection(accToken) {
             firstTranslation = false;
         }
     
-            var elem = document.createTextNode(response.recognition + '(' + response.translation + ')');
-            var hr = document.createElement('hr');
-            var parent = document.getElementById("output");
-            parent.appendChild(hr);
-            parent.appendChild(elem);
+            this.setState({transcript: response.recognition + '(' + response.translation + ')'});
+            this.render();
+            // var hr = document.createElement('hr');
+            // var parent = document.getElementById("output");
+            // parent.appendChild(hr);
+            // parent.appendChild(elem);
     }
     
-    mediaRecorder.ondataavailable = function (blob) {
-        if (ws)
-            ws.send(blob);
+    this.state.mediaRecorder.ondataavailable = (blob) => {
+      console.log('data available, sending blob');
+      if (this.state.ws)
+          this.state.ws.send(blob);
     };
-}
+  }
 
-
-
-function convertToArray(langArray) {
+  convertToArray(langArray) {
     var arr = Object.keys(langArray).map(function (k) { let obj = langArray[k]; obj.key = k; return obj; });
     return arr;
-}
+  }
 
-function findMatch(userLang, langArray) {
+  findMatch(userLang, langArray) {
     for (var i = 0; i < langArray.length; i++) {
         if (userLang == langArray[i].key.substring(0, 2)) {
             return langArray[i];
         }
     }
     return false;
-}
+  }
 
 //This function is called on page load
 //The purpose of this function is to enumerate all speech recognition and translation languages
-function getLang() {
+  getLang() {
     var langURL = "https://dev.microsofttranslator.com/languages?scope=speech,text&api-version=1.0";
     
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
+    xmlhttp.onreadystatechange = () => {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var results = JSON.parse(xmlhttp.responseText);
             var sourceLangArray = convertToArray(results.speech);
@@ -266,32 +163,31 @@ function getLang() {
             var userLang = (navigator.language).substring(0, 2);
             var match1 = findMatch(userLang, sourceLangArray);
             if (match1) {
-                sourceLang = match1.key;
-                sourceLangName = match1.name;
+                this.state.sourceLang = match1.key;
+                this.state.sourceLangName = match1.name;
             }
             var match2 = findMatch(userLang, targetLangArray);
             if (match2) {
-                targetLang = match2.key;
-                targetLangName = match2.name;
+                this.state.targetLang = match2.key;
+                this.state.targetLangName = match2.name;
             }
             
             //save the languages
-            chrome.storage.local.set({ 'sourceLanguage': sourceLang, 'targetLanguage': targetLang, 'sourceLanguageName': sourceLangName, 'targetLanguageName': targetLangName }, function () {
+            chrome.storage.local.set({ 'sourceLanguage': this.state.sourceLang, 'targetLanguage': this.state.targetLang, 'sourceLanguageName': this.state.sourceLangName, 'targetLanguageName': this.state.targetLangName }, function () {
                 if (chrome.runtime.lastError) {
                     Logger.error("Failed to save the selected options at runtime", chrome.runtime.lastError);
                 }
             });
-            console.log({ 'sourceLanguage': sourceLang, 'targetLanguage': targetLang, 'sourceLanguageName': sourceLangName, 'targetLanguageName': targetLangName });
+            console.log({ 'sourceLanguage': this.state.sourceLang, 'targetLanguage': this.state.targetLang, 'sourceLanguageName': this.state.sourceLangName, 'targetLanguageName': this.state.targetLangName });
         }
     }
     xmlhttp.open("GET", langURL, true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send();
-}
-
+  }
 
 //The purpose of this function is to display an error to the user based on the scenario
-function displayError(errorCode) {
+  displayError(errorCode) {
     
     var messageText = "";
     var messageString = "";
@@ -337,4 +233,84 @@ function displayError(errorCode) {
     
     console.error(debuggingString, messageText, messageString);
     
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.StartSession.bind(this)}>Start</button>
+        <button onClick={this.StopSession.bind(this)}>Stop</button>
+        <div className='output'>{this.state.transcript}</div>
+      </div>
+    )
+  }
 }
+
+export default Transcriber;
+
+/*
+function getAdmToken(){
+    var post_data = querystring.stringify({
+        'client_id': 'invalid-memories', //your client id,
+        'scope': 'http://api.microsofttranslator.com',
+        'grant_type': 'client_credentials',
+        'client_secret': '5Bpf0b5tDzFUGiFNL8lyLJjEyQKUvN2pQo2j8piaxvo=' // your client secret
+    });
+    
+    var post_options = {
+        host: 'datamarket.accesscontrol.windows.net',
+        path: '/v2/OAuth2-13',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(post_data)
+        }
+    };
+    
+    
+    var post_req = https.request(post_options, function (res) {
+        res.setEncoding('utf8');
+        if (res.statusCode == 200) {
+            res.on('data', function (chunk) {
+                accToken = JSON.parse(chunk).access_token;
+                //log('[generateAdmToken] Success');
+            });
+        }
+        else {
+            accToken = ERR_REQ_FAIL;
+            //log('[generateAdmToken] Failed with status code ' + res.statusCode, "error");
+        }
+    });
+    
+    post_req.write(post_data);
+    post_req.end();
+
+}
+
+function GenerateToken(callbackIfSuccessfulToken) {
+    var accToken = "";
+
+    var xhttp = new XMLHttpRequest();
+    // *** USE YOUR OWN SERVER TO RETURN A VALID ADM TOKEN ***
+    // We suggest using a session cookie for a minimal validation that request for token is coming from your own client app
+    // For commercial apps, you may want to protect the call to get a token behind your own user authentication.
+    // xhttp.open("GET", "http://localhost:3000/token", true);
+    xhttp.open('GET', 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken', true);
+    
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            accToken = xhttp.responseText;
+
+            console.debug("Token: " + accToken);
+            if (typeof callbackIfSuccessfulToken === 'function')
+                callbackIfSuccessfulToken(accToken);
+            else
+                console.error("callbackIfSuccessfulToken is not a function");
+        }
+    }
+    
+    xhttp.send();
+
+}
+*/
