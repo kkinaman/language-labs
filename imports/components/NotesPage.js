22f5219db70cb3c4816117f78b25bdfb95085ee8
Karina Kinaman <kkinaman419@gmail.com>
2016-11-05 12:17:29 -0700

    (feat) Add dropdown on Notes Page which allows user to view their flashcards from any language

--- imports/components/NotesPage.js
+++ imports/components/NotesPage.js
@@ -5,13 +5,29 @@ import _ from 'lodash';
 class NotesPage extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      displayedLanguage: _.capitalize(this.props.user.profile.learning)
+    }
+  }
+
+  changeDisplayedLanguage(event) {
+    this.setState({
+      displayedLanguage: event.target.value
+    });
   }
 
   render() {
     return (
       <div className='notes-page'>
         <NavigationWrapper returnToNav={ this.props.returnToNav } />
-          <h1>Notes</h1>  
+          <h1>Notes</h1> 
+
+          <select defaultValue={_.capitalize(this.props.user.profile.learning)}
+                  onChange={this.changeDisplayedLanguage.bind(this)}>
+            <option value="French">French</option>
+            <option value="Spanish">Spanish</option>
+            <option value="German">German</option>
+          </select>
         <div className='notes-main'>
           <div className='notes-container shadowbox'>
             <div className='notes shadowbox'>
@@ -34,18 +50,18 @@ class NotesPage extends React.Component {
                 <thead>
                   <tr>
                     <th>{_.capitalize(this.props.user.profile.language)}</th>
-                    <th>{_.capitalize(this.props.user.profile.learning)}</th>
+                    <th>{_.capitalize(this.state.displayedLanguage)}</th>
                   </tr>
                 </thead>
                 <tbody>
                   {
                     this.props.notes.map(note => {
                       if (note.noteType === 'flashcard'  && 
-                        Object.keys(note.text).indexOf(this.props.user.profile.learning.toLowerCase()) !== -1) {
+                        Object.keys(note.text).indexOf(this.state.displayedLanguage.toLowerCase()) !== -1) {
                         return (
                           <tr>
                             <td><text>{note.text[this.props.user.profile.language.toLowerCase()]}</text></td>
-                            <td><text>{note.text[this.props.user.profile.learning.toLowerCase()]}</text></td>
+                            <td><text>{note.text[this.state.displayedLanguage.toLowerCase()]}</text></td>
                           </tr>
                         )
                       }
